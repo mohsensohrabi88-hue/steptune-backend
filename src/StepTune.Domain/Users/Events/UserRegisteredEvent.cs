ab446ce54bb837d17abba0582c00cfee2cc72528
mohsenSohrabi <mohsen.sohrabi.cs@gmail.com>
2025-12-08 11:26:18 +0330

    feat: provide UserRegisterEvent

--- src/StepTune.Domain/Users/Events/UserRegisteredEvent.cs
+++ src/StepTune.Domain/Users/Events/UserRegisteredEvent.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using StepTune.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace StepTune.Domain.Users.Events
 {
-    public class UserRegisteredEvent
-    {
-    }
+    public sealed record UserRegisteredEvent(Guid UserId):DomainEvent;
+    
 }
