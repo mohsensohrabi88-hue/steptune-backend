dfc9063d46309b75ea286fff61c92c080e9e145f
mohsenSohrabi <mohsen.sohrabi.cs@gmail.com>
2025-12-08 11:54:31 +0330

    feat:implement ProfileCompletion Service

--- src/StepTune.Domain/Users/Services/ProfileCompletionService.cs
+++ src/StepTune.Domain/Users/Services/ProfileCompletionService.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using StepTune.Domain.Users.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace StepTune.Domain.Users.Services
 {
-    public class ProfileCompletionService
+    public sealed class ProfileCompletionService
     {
+        public int CacluateCompletion(UserProfile profile)
+        {
+            int score = 0;
+            int total = 6;
+
+            if (!string.IsNullOrEmpty(profile.FirstName)) score++;
+            if (!string.IsNullOrEmpty(profile.LastName)) score++;
+            if(profile.BirthDate != null) score++;
+            if (profile.Gender != null) score++;
+            if(profile.WeightKg != null) score++;
+            if(profile.HeightCm != null) score++;
+
+            return (int) Math.Round((double) score / total * 100);
+                
+        }
+
+        
     }
 }
